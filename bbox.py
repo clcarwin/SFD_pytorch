b8d8e4bc259de379876b154e351b34c63182b849
Leo Dirac <deepembedding@gmail.com>
2019-10-20 12:40:49 -0700

    Cleaning up py3 & old torch.  Moving bbox functions from numpy to pytorch.

--- bbox.py
+++ bbox.py
@@ -34,10 +34,12 @@ def nms(dets, thresh):
     if 0==len(dets): return []
     x1,y1,x2,y2,scores = dets[:, 0],dets[:, 1],dets[:, 2],dets[:, 3],dets[:, 4]
     areas = (x2 - x1 + 1) * (y2 - y1 + 1)
-    order = scores.argsort()[::-1]
+    scores = np.asarray(scores)
+    order = scores.argsort()
+    order = np.asarray(list(order[::-1]))
 
     keep = []
-    while order.size > 0:
+    while len(order) > 0:
         i = order[0]
         keep.append(i)
         xx1,yy1 = np.maximum(x1[i], x1[order[1:]]),np.maximum(y1[i], y1[order[1:]])
